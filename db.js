b99d75ba82e21f24de656837e86f05b1cffbf1b0
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-03-02 21:04:55 -0500

    work already

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const pg = require("pg");
-const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_db"
-);
+// const client = new pg.Client(
+//   process.env.DATABASE_URL || "postgres://localhost/acme_db"
+// );
 
-client.connect();
+// client.connect();
 
 const sync = async () => {};
 
--- dist/App.js
+++ dist/App.js
@@ -2,22 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axious.get("/api/users").then(response => setUsers(response.data));
-  }, []);
-
-  return (
-    <div>
-      <div>My Template</div>
-      <ul>
-        {users.map(user => {
-          return <li key={user.id}>{user.name}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  return <div>My Template</div>;
 };
 
 export default App;
